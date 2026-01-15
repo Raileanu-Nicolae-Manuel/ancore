4a51ae4defe5a369334154aec2e5c3f46ed2ee7a
Manuel <manuelraileanu@gmail.com>
2026-01-15 20:19:48 +0200

    set-up the ui to be share from packages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,12 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
@@ -13,4 +15,10 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  optimizeDeps: {
+    // This prevents Vite from pre-bundling your UI library.
+    // It forces Vite to treat it as source code, 
+    // allowing Tailwind to scan the .tsx files.
+    exclude: ["@ancore/ui"],
+  },
 })
---
+++ packages/ui/src/components/test.component.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./ui/button";
+
+export default function TestComponent() {
+  return (  
+    <div className="flex h-screen items-center justify-center">
+      <Button>Click Me</Button>
+    </div>
+  );
+}
